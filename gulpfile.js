5855cc0505c1ceafcd2a30d5638e1798a85065ad
Samurais <hailiang.hl.wang@gmail.com>
2016-05-28 11:33:09 +0800

    lightweight gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,80 +16,51 @@ var replace = require('gulp-replace');
 
 // === PATHS
 var paths = {
-  sass: ['./src/index.scss', './src/**/*.scss'],
-  jade: ['./src/**/*.jade', './src/index.jade'],
-  js: ['./src/**/*.js']
+    sass: ['./scss/**/*.scss'],
+    js: ['./www/**/*.js']
 };
 
 // === DEFAULT TASK
 gulp.task('default', ['serve']);
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'compressJs', 'jade-index', 'jade-templates'], function() {
+gulp.task('serve', ['sass'], function() {
 
-  browserSync.instance = browserSync.init({
-    startPath: '/index.html',
-    server: {
-      baseDir: "./www",
-      middleware: [
-        historyApiFallback()
-      ]
-    },
-    port: 8100
-  });
+    browserSync.instance = browserSync.init({
+        startPath: '/index.html',
+        server: {
+            baseDir: "./www",
+            middleware: [
+                historyApiFallback()
+            ]
+        },
+        port: 8100
+    });
 
-  // === WATCH
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.js, ['compressJs', browserSync.reload]);
-  gulp.watch(paths.jade[1], ['jade-index', browserSync.reload]);
-  gulp.watch(paths.jade[0], ['jade-templates', browserSync.reload]);
+    // === WATCH
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch("www/**/*").on('change', browserSync.reload);
+    // gulp.watch(paths.js, [browserSync.reload]);
 
-  gutil.log(gutil.colors.red('\n\n•••••••••••••••••••••••••••••••••••••\n'), gutil.colors.yellow('  SERVER RUNNING...'), gutil.colors.red('\n•••••••••••••••••••••••••••••••••••••\n\n'));
+    gutil.log(gutil.colors.green('\n\n•••••••••••••••••••••••••••••••••••••\n'),
+        gutil.colors.yellow('  SERVER RUNNING...'),
+        gutil.colors.green('\n•••••••••••••••••••••••••••••••••••••\n\n'));
 
-	// gulp.watch("www/**/*").on('change', browserSync.reload);
 });
 
 // === COMPILE TASKS
-gulp.task('sass', function () {
-  return gulp.src(paths.sass)
-  	.pipe(concat('style.scss'))
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('compressJs', function () {
-  return gulp.src(paths.js)
-    .pipe(ngAnnotate())
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./www/js/'));
-});
-
-
-gulp.task('jade-templates', function(){
-	return gulp.src(paths.jade[0])
-    .pipe(jade())
-    .pipe(gulp.dest('./www/templates/'));
-})
-gulp.task('jade-index', function () {
-  return gulp.src(paths.jade[1])
-    .pipe(jade())
-    .pipe(gulp.dest('./www/'));
-});
-
-
-gulp.task('build:device', ['sass', 'compressJs', 'jade-templates'],function () {
-  return gulp.src(paths.jade[1])
-  	.pipe(replace('base(href="/")', 'base(href=".")'))
-    .pipe(jade())
-    .pipe(gulp.dest('./www/'));
+gulp.task('sass', function() {
+    return gulp.src(paths.sass)
+        .pipe(concat('style.scss'))
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(browserSync.stream());
 });
